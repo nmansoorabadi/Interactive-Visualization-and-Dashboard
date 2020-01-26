7a2c0a7191fb3a03f19a5ce4243b0cc974cedbef
Najmeh dehghani <55862735+nmansoorabadi@users.noreply.github.com>
2020-01-26 03:06:31 -0600

    Add files via upload

--- Dashboards/app.js
+++ Dashboards/app.js
@@ -1,119 +1,105 @@
     
- function init(selData){   
+  
     // Create the metaData
     function metaData (sample){
-        d3.json("data/samples.json").then(data => {
+        d3.json("data/samples.json").then((data) => {
             console.log(data);
-           var  metaData=[data.metadata.data];
-
-            const panel= d3.select("#sample-metadata");
-            // panel.html("");
-//             Object.entries(metaData).forEach(([key, value]) =>{
-//             const row=panel.append("option")
-//             row.text(`${key}:${value}`);
+            const mData=data.metadata;
+            d3.select("#sample-metadata").html("");
+            Array.from(mData).forEach(item => {
+                const id=mData.id;
+                const ethnicity=mData.ethnicity;
+                const gender=metaData.gender;
+                const location=mData.location;
+                const bbtype=mData.bbtype;
+                const wfreq=mData.wfreq;
+// //             Object.entries(mData).forEach(([key, value]) =>{
+// //             const row=panel.append("option")
+// //             row.text(`${key}:${value}`);
           
-            Array.from(metaData).forEach(data => {
-            d3.select("#sample-metadata").append('option').attr('value',data).text(data);
+//             Array.from(mData).forEach(data => {
+//             d3.select("#sample-metadata").append('option').attr('value',data).text(data);
             });
             
         });
     }
 
-metaData('940');
+metaData();
+       
+    function genPlot(selSample){
 
-
-
-
-
-// Use d3.json() to read the json file and create the metaData
-
-    d3.json("data/samples.json").then(data => {
-        console.log(data);
-        // Read the dropdown class
-        const dataSet=data.names;
-        d3.select("#selDataset");
-        Array.from(dataSet).forEach(names => {
-        d3.select("#selDataset").append('option').attr('value', names).text(names);
-        });
-    });
-  
-        
+        d3.json("data/samples.json").then(data => {
             
-        
-    function genPlot(){
-            d3.json("data/samples.json").then(data => {
-            d3.select("#selDataset").node().value =selData;
-            const selSample=data.samples;
-            const filData=selSample.filter(item => item.samples === selData);
-            const sample_values=filData.map(item => item.sample_values).slice(0,10);
-            const otu_ids= filData.map(item => item.otu_ids).slice(0,10);
-            const otu_labels=filData.map(item => item.otu_labels).slice(0,10);
+             // Read the dropdown class
+            const dataSet=data.names;
+            
+            d3.select("#selDataset").html("");
+            Array.from(dataSet).forEach(names => {
+                d3.select("#selDataset").append('option').attr('value', names).text(names);
+            });
+                
+               
+            const chartSample= data.samples;
+            inputData=chartSample.filter(item => item.id === selSample);
+            const filData=inputData.slice(0,10);
             // Create the horizontal bar chart
             // Use sample_values as the values for the bar chart.
             // Use otu_ids as the labels for the bar chart.
             // Use otu_labels as the hovertext for the chart.
 
-            const barTrace=[
-            {
-            x:sample_values,
-            y:otu_ids,
-            // hovertext:otu_labels.slice(0,10),
-            type:'bar',
-            orientation: 'h'
-            }];
-
-            const barlayout={
-            title: "Belly Button Bacteria",
-            xaxis: {title: "Sample Values"},
-            yaxis: {title: "OTU ID"}
+            const barTrace={
+                x:filData.map(item => item.sample_values),
+                y:filData.map(item => item.otu_ids),
+                text:filData.map(item => item.otu_labels),
+                type:'bar',
+                orientation: 'h'
+            },
+            layout={
+                title: "Belly Button Bacteria",
+                xaxis: {title: "Sample Values"},
+                yaxis: {title: "OTU ID"}
             };
 
+            Plotly.newPlot('bar',[barTrace], layout);
 
-            Plotly.newPlot('bar',barTrace, barlayout);
-
-
+    
             // bubble chart
             // Use otu_ids for the x values.
             // Use sample_values for the y values.
             // Use sample_values for the marker size.
             // Use otu_ids for the marker colors.
             // Use otu_labels for the text values 
-            const sampleVal=filData.map(item => item.sample_values);
-            const otdID= filData.map(item => item.otu_ids);
-            const otdLable=filData.map(item => item.otu_labels);
-
-
-            const bubbleTrace=[
-                {
-                x:otdID,
-                y:sampleVal,
+    
+            const bubbleTrace={
+                x:inputData.map(item => item.otu_ids),
+                y:inputData.map(item => item.sample_values),
                 mode:'markers',
                 marker:{
-                    size:sampleVal,
-                    color: otdID,
+                    size:inputData.map(item => item.sample_values),
+                    color: inputData.map(item => item.otu_ids),
                 },
-                text:otu_labels,
-                // type:'scatter',
-                
-            }];
-            const layoutBubble={
+                text:inputData.map(item => item.otu_labels),   
+            },
+            
+
+            layoutBubble={
                 title: "Belly Button Bacteria",
                 xaxis: {title: "OTU ID"},
                 yaxis: {title: "Sample Values"}
             };
-            Plotly.newPlot('bubble',bubbleTrace,layoutBubble);
+            Plotly.newPlot('bubble',[bubbleTrace],layout);
 
-        }); 
+        });
 
     } 
            
-    genPlot();   
-}
+    genPlot('940');   
 
-init('940');
 
-d3.select('#selDataset').on('change', () =>{
-    init(d3.event.target.value);
+
+
+d3.select('#selDataset').on('change', () => {
+    genPlot(d3.event.target.value);
 
 });
 
